8aa4254cdc66f0cb939da7fc6edcbe770b33afe5
YuanGao0702 <gaoyuan0702@gwu.edu>
2015-10-08 03:01:59 -0400

    Update Event.java

--- POJO/Event.java
+++ POJO/Event.java
@@ -13,59 +13,86 @@ public class Event {
 	private ArrayList<User> passengers;
 	private ArrayList<User> pending;
 	private int capacity;
+	private String description;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public Date getStartTime() {
 		return startTime;
 	}
+
 	public void setStartTime(Date startTime) {
 		this.startTime = startTime;
 	}
+
 	public Date getEndTime() {
 		return endTime;
 	}
+
 	public void setEndTime(Date endTime) {
 		this.endTime = endTime;
 	}
+
 	public String getStartAddress() {
 		return startAddress;
 	}
+
 	public void setStartAddress(String startAddress) {
 		this.startAddress = startAddress;
 	}
+
 	public String getEndAddress() {
 		return endAddress;
 	}
+
 	public void setEndAddress(String endAddress) {
 		this.endAddress = endAddress;
 	}
+
 	public User getDriver() {
 		return driver;
 	}
+
 	public void setDriver(User driver) {
 		this.driver = driver;
 	}
+
 	public ArrayList<User> getPassengers() {
 		return passengers;
 	}
+
 	public void setPassengers(ArrayList<User> passengers) {
 		this.passengers = passengers;
 	}
+
 	public ArrayList<User> getPending() {
 		return pending;
 	}
+
 	public void setPending(ArrayList<User> pending) {
 		this.pending = pending;
 	}
+
 	public int getCapacity() {
 		return capacity;
 	}
+
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
 	}
-	
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 }
