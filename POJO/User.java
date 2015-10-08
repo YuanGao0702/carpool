184a8de4f20987e8e9e155c02fb702efa682a9eb
YuanGao0702 <gaoyuan0702@gwu.edu>
2015-10-08 02:32:58 -0400

    Update User.java

--- POJO/User.java
+++ POJO/User.java
@@ -6,4 +6,23 @@ public class User {
 	private String username;
 	private String password;
 	private ArrayList<Event> events;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public ArrayList<Event> getEvents() {
+		return events;
+	}
+	public void setEvents(ArrayList<Event> events) {
+		this.events = events;
+	}
+	
 }
