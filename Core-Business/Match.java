b16c05adce294005c5f4c34baffd347769bb9c11
YuanGao0702 <gaoyuan0702@gwu.edu>
2015-10-08 03:07:56 -0400

    Create Match.java

---
+++ Core-Business/Match.java
@@ -0,0 +1,13 @@
+package FullStack.Java.GWUCarpool;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Match {
+	public ArrayList<Event> match(Date startTime, Date endTime,
+			String startAddr, String endAddr) {
+		// TODO with Google Map API
+
+		return null;
+	}
+}
