b3fd664a4295caf45bf425d19899a92cbfde495d
yifan <zyfacatch@gmail.com>
2015-12-01 01:32:42 -0500

    add cookies

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -1,9 +1,10 @@
+'use strict';
 
-
-var gwuCarpool = angular.module('gwuCarpool', ['ngRoute']);
+var gwuCarpool = angular.module('gwuCarpool', ['ngRoute', 'carpoolCtrl']);
 
    // configure our routes
-   gwuCarpool.config(function($routeProvider) {
+   gwuCarpool.config(['$routeProvider',
+                      function($routeProvider) {
        $routeProvider
 
            // route for the home page
@@ -43,126 +44,8 @@ var gwuCarpool = angular.module('gwuCarpool', ['ngRoute']);
            controller  : 'riderInfoController'
            })
        
-   });
+   }]);
 
-   // create the controller and inject Angular's $scope
-   gwuCarpool.controller('mainController', function($scope) {
-     
-   });
-
-   gwuCarpool.controller('aboutController', function($scope) {
-       $scope.message = 'Look! I am an about page.';
-   });
-
-   gwuCarpool.controller('loginController', function($scope, $http, $cookieStore) {
-		 	console.log("loginController");
-
-		 	$scope.login = function() {
-		 		console.log($scope.user);
-		 		console.log("login");
-		 		$http.get('http://localhost:8080/api/user/liuqi627@gwu.edu', $scope.user).success(function (response) {
-		 			console.log(response);
-		 			$scope.user = response;
-		 			$cookieStore.put('user', $scope.user);	 					 			
-		 		});
-		 	};
-   });
    
-   gwuCarpool.controller('signupController', function($scope, $http, $cookieStore, $window) {
-	 	console.log("signupController");
-
-	 	$scope.signup = function() {
-	 		console.log($scope.user);
-	 		console.log("signup");
-	 		$http.get('user/event.json', $scope.user).success(function (response) {
-	 			console.log(response);
-	 			$scope.user = response;
-	 			$cookieStore.put('user', $scope.user);
-	 			window.location.href = "#/account";
-	 		});
-	 	};
-});
-   
-   gwuCarpool.controller('accountController', function($scope, $http, $cookieStore) {
-	 	console.log("accountController");
-	 	$scope.user = $cookieStore.get('user');						 				 	
-   });
-   
-   gwuCarpool.controller('driverInfoController', function($scope, $http) {
-	 	console.log("driverInfoController");
-
-	 	$scope.submit = function() {
-	 		console.log($scope.driver);
-	 		console.log("submit driver info");
-	 		$http.post('', $scope.driver).success(function (response) {
-	 			console.log(response);
-	 			$scope.driver = response;
-	 			
-	 		});
-	 	};
-   });
-   
-   gwuCarpool.controller('riderInfoController', function($scope, $http) {
-	 	console.log("riderInfoController");
-	 	
-	 	$scope.search = function() {
-	 		console.log($scope.rider);
-	 		console.log("search event");
-	 		$http.get('js/event.json').success(function(data) {
-	 			$scope.events = data;  
-		 		   console.log(data);	 		   
-	 		  });
-
-//	 		$http.get('', $scope.rider).success(function (response) {
-//	 			console.log(response);
-//	 			$scope.results = response;
-//	 			window.location.href = "#/results";
-//	 		});
-	 	};
-	 	
-	 	
-   });
-   
-
-// var gwuCarpool = angular.module('gwuCarpool',[]);
-
-// gwuCarpool.config(['$httpProvider', function ($httpProvider) {
-//     $httpProvider.defaults.useXDomain = true;
-//     $httpProvider.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-//     delete $httpProvider.defaults.headers.common['X-Requested-With'];
-// }]);
-
-// // Added all header request and response.
-// //app.all('/*', function (request, response, next) {
-// //    response.header("Access-Control-Allow-Origin", "*");
-// //    response.header("Access-Control-Allow-Headers", "X-Requested-With");
-// //    response.header("Access-Control-Allow-Methods", "GET, POST", "PUT", "DELETE");
-// //    next();
-// //
-// //});
-
-// gwuCarpool.controller('mainController', ['$scope', '$http',
-// 	function ($scope, $http) {
-// 	console.log("Hello world from controller.js");
-
-// 	$scope.login = function() {
-// 		console.log($scope.user);
-// 		console.log("loginangularjs");
-// 		$http.get('http://localhost:8080/api/user/liuqi627@gwu.edu', $scope.user).success(function (response) {
-// 			console.log(response);
-// 			$scope.user = response;
-// 		});
-// 	};
-	
-// 	$scope.getUser = function() {
-// 		console.log($scope.user);
-// 		console.log("getUserangularjs");
-// 		$http.get('/getUser', $scope.user).success(function (response) {
-// 			console.log(response);
-// 		});
-// 	};
-
-// }]);
-
 
 
